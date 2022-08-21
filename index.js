b070569230d11564b96d62a8667ed2e7d655353e
+ <+>
2022-08-21 23:51:25 +0300

    js- strings lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+// initialize current user
+const currentUser = 'Grace Hopper';
+//welcome message
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//upper case concatenated
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+//trim the string with a welcome message
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`; 
+
